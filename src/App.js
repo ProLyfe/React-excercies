13507e872286ede41e580018101ff9aa7c1566e9
ProLyfe <rayane.prolyfe@gmail.com>
2021-02-02 16:31:51 +0100

    add

--- src/App.js
+++ src/App.js
@@ -1,8 +1,3 @@
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Home from './components/Home';
-import About from './components/About';
-import Users from './components/Users';
-import Navbar from './components/Navbar';
 import Routeur from "./components/Route/Routeur";
 import NoMatch from "./components/404";
 
